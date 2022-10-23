a8c4c4ceb20ad479d3a68f921f46a45e4b66c9c9
tuannt <tuannt@tokyotechlab.com>
2022-10-23 23:32:15 +0700

    update code

--- index.js
+++ index.js
@@ -2,18 +2,69 @@ var puppeteer = require('puppeteer');
 const excelToJson = require('convert-excel-to-json');
 var _ = require('lodash');
 
-const result = excelToJson({
-	sourceFile: './input/testcase.xlsx',
-  header:{
-    rows: 1
-  },
-  columnToKey: {
-		A: 'action',
-		B: 'element',
-		C: 'value',
-	}
-});
+// const result = excelToJson({
+// 	sourceFile: './input/testcase.xlsx',
+//   header:{
+//     rows: 1
+//   },
+//   columnToKey: {
+// 		A: 'action',
+// 		B: 'element',
+// 		C: 'value',
+// 	}
+// });
 const BASE_URL = 'http://localhost:8088'
+
+// test data
+const result  = {
+  Login: [
+    { action: 'open' },
+    {
+      action: 'wait',
+      element: '//*[@id="root"]/section/section/main/div/div/form/div[1]/div/div/div/div[2]/div[1]/div/input'
+    },
+    {
+      action: 'type',
+      element: '//*[@id="root"]/section/section/main/div/div/form/div[1]/div/div/div/div[2]/div[1]/div/input',
+      value: 'tuannt@tokyotechlab.com'
+    },
+    {
+      action: 'type',
+      element: '//*[@id="root"]/section/section/main/div/div/form/div[2]/div/div/div/div[2]/div[1]/div/input',
+      value: 'Center@1234'
+    },
+    {
+      action: 'click',
+      element: '//*[@id="root"]/section/section/main/div/div/form/div[4]/div/div/button'
+    },
+    {
+      action: 'wait',
+      element: '//*[@id="root"]/section/aside[1]/div/div[1]'
+    },
+    { action: 'delay', value: 2000 },
+    { action: 'screenshot', value: 'dashboard' }
+  ],
+  CreateBookingFromListt: [
+    { action: 'open', value: '/room-booking' },
+    {
+      action: 'click',
+      element: '//*[@id="staff-layout-content"]/div/main/div/div/div[1]/button'
+    },
+    {
+      action: 'delay',
+      element: 3000
+    },
+    {
+      action: 'click',
+      element: '/html/body/div[5]/div/div[2]/div/div[2]/div/div/div[2]/div[1]/form/div[1]/div/div/div/div[2]/div[1]'
+    },
+    {
+      action: 'type',
+      element: '/html/body/div[5]/div/div[2]/div/div[2]/div/div/div[2]/div[1]/form/div[1]/div/div/div/div[2]/div[1]/div/div/div/span[1]/input',
+      value: 'single'
+    }
+  ]
+}
 
 console.log(result)
 
@@ -64,6 +115,7 @@ async function init() {
       return page.click(item.element)
     }
     const elements = await page.$x(item.element)
+    console.log('elements.length', elements.length);
     if(!elements.length) return
     await elements[0].click()
   }
